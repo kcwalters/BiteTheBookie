6df06155e6b901d0b58dad5ca126535a36b22d53
Kevin Walters <kcwalters101@gmail.com>
2026-07-17 13:03:10 -0400

    Update test

--- BiteTheBookie/Program.cs
+++ BiteTheBookie/Program.cs
@@ -14,7 +14,7 @@ using Microsoft.EntityFrameworkCore;
 using OpenAI.Chat;
 
 var builder = WebApplication.CreateBuilder(args); 
-
+ 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
